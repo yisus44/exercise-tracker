3865186081372b57ad78a5df852d9ab2d939d555
Jesus <jesusadrian1953.1@gmail.com>
2021-05-22 12:42:51 -0500

    added cors for development

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 
 const userRoutes = require("./user/user.routes.js");
+const cors = require("cors");
+
+if ((process.env.NODE_ENV = "development")) {
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
+} else {
+  app.use(cors({ credentials: true }));
+}
 
 app.use(express.json());
 
